79c07994956b82828c66d135a86fac11dba225bb
Roshan <roshanraj.ui499@gmail.com>
2026-05-14 23:16:03 +0530

    landing page setup completed

--- src/app/layout/main-layout/main-layout.ts
+++ src/app/layout/main-layout/main-layout.ts
@@ -4,10 +4,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { Footer } from '../../shared/components/footer/footer';
 
 @Component({
   selector: 'app-main-layout',
-  imports: [RouterModule, MatButtonModule, MatIconModule, MatMenuModule, MatSidenavModule],
+  imports: [RouterModule, MatButtonModule, MatIconModule, MatMenuModule, MatSidenavModule, Footer],
   templateUrl: './main-layout.html',
   styleUrl: './main-layout.scss',
 })
