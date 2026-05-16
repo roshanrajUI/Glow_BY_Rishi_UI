516f8ad5fb08c58eecf270d05c752b0316512704
Roshan <roshanraj.ui499@gmail.com>
2026-05-17 00:15:25 +0530

    implemented scrolling

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,14 +3,24 @@ import {
   provideBrowserGlobalErrorListeners,
   provideZoneChangeDetection,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+
+    provideZoneChangeDetection({
+      eventCoalescing: true,
+    }),
+
+    provideRouter(
+      routes,
+      withInMemoryScrolling({
+        anchorScrolling: 'enabled',
+        scrollPositionRestoration: 'enabled',
+      }),
+    ),
   ],
 };
