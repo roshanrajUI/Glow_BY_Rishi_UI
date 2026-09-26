9646b2b5228446bc9b9b75947f8cc430384518f4
Roshan <roshanraj.ui499@gmail.com>
2026-09-26 23:45:05 +0530

    fix: update API base URL to use HTTPS for secure connections

--- src/app/constants/rest-url.ts
+++ src/app/constants/rest-url.ts
@@ -1,6 +1,6 @@
 export const API_URL = {
   // BASEURL: 'http://localhost:3000/api',
-  BASEURL: 'http://api.glowbyrishi.in/api',
+  BASEURL: 'https://api.glowbyrishi.in/api',
   // BASEURL: 'https://glow-by-rishi-api.onrender.com/api',
   CATEGORIES: 'categories',
   GETCATEGORIES: 'categories/all',
